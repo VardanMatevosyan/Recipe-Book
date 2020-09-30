d3b66683e9587b3a8f2231311a6cd7aadb0f0f45
VardanMatevosyan <vardanmt@gmail.com>
2020-09-30 10:06:42 +0300

    8jpfy3 - added dynamic alert using *ngIf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {AuthComponent} from './auth/auth.component';
 import {LoadingSpinnerComponent} from './shared/loading-spinner/loading-spinner.component';
 import {AuthInterceptorService} from './auth/auth-interceptor.service';
+import {AlertComponent} from './shared/alerts/alert.component';
 
 
 @NgModule({
@@ -37,7 +38,8 @@ import {AuthInterceptorService} from './auth/auth-interceptor.service';
     RecipeStartComponent,
     RecipeEditComponent,
     AuthComponent,
-    LoadingSpinnerComponent
+    LoadingSpinnerComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
